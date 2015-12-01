69964de9db964d129761ddcb9ccefee07607ec37
Eric Wieser <wieser.eric@gmail.com>
2015-12-01 13:00:04 -0500

    Add a rail class

---
+++ software/Rail.m
@@ -0,0 +1,69 @@
+classdef Rail
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        left
+        right
+        
+        init_pos_right = -15803;
+        init_pos_left = 1102;
+    end
+    
+    methods
+        function obj = Rail(conn)
+            obj.right = conn.mxmotor(1);
+            obj.left = conn.mxmotor(2);
+            
+            max_torque = 500;
+            right.MaxTorque = max_torque;
+            left.MaxTorque = max_torque;
+            right.TorqueLimit = max_torque;
+            left.TorqueLimit = max_torque;
+
+            right.MovingSpeed = 1023;
+            left.MovingSpeed = 1023;
+        end
+        
+        function setPoses(obj, pos)
+            obj.right.GoalPosition = obj.init_pos_right + pos(1);
+            obj.left.GoalPosition = obj.init_pos_left - pos(2);
+        end
+        function pos = getPoses(obj)
+            pos = [
+                obj.right.PresentPosition - obj.init_pos_right;
+                obj.init_pos_left - obj.left.PresentPosition;
+            ];
+        end
+        
+        function obj = setOrigin(obj)
+            obj.init_pos_right = obj.right.PresentPosition;
+            obj.init_pos_left = obj.left.PresentPosition;
+        end
+        
+        function raiseRel(obj, amt)
+            p = obj.getPoses();
+            obj.setPoses(p+amt);
+        end
+        
+        function tiltRel(obj, amt)
+            p = obj.getPoses();
+            obj.setPoses(p+[1;-1]*amt);
+        end
+        
+        % high level commands
+        function lower(obj)
+            obj.setPoses([0; 0]);
+        end
+        function raise(obj)
+            obj.setPoses([20500; 20500]);
+        end
+        function dumpSnow(obj)
+            obj.setPoses([20500; 27900]);
+        end
+        function dumpDebris(obj)
+            obj.setPoses([27900; 20500]);
+        end
+    end
+end
+
