7cd25e1e33c9a82ccd1cc48689f88fd1c85ec4a8
Team <212-a-team@mit.edu>
2015-12-03 16:34:01 -0500

    Merge branch 'master' of https://github.com/eric-wieser/2.12-robot
    Conflicts:
    	software/DynamixelConn.m

--- software/Tasks.m
+++ software/Tasks.m
@@ -15,40 +15,42 @@ classdef Tasks < handle
             obj.nav = nav;
         end
         
-        function taskone= mt_simmons()
-        mt_simmons_x = 1.25;
-        mt_simmons_y = .5;
-
+                
+        function taskone= mt_simmons(obj)
+        mt_simmons_x = 1.219;
+        mt_simmons_y = .33;
+        
         obj.nav.gpsFeedbackNav(obj.gps, mt_simmons_x, mt_simmons_y);
+        
 
-        % arm.sweepSnow();
-        % arm.outoftheway();
-        % rail.dumpSnow();
-        % rail.wiggle();
-        % rail.dumpDebris();
-        % rail.wiggle();
+        %arm.sweepSnow();
+        %arm.outoftheway();
+        %rail.dumpSnow();
+        %rail.wiggle();
+        %rail.dumpDebris();
+        %rail.wiggle();
         end
         
-        function tasktwo = tree()
-        tree_x = 0;
-        tree_y = 0;
+        function tasktwo = tree(obj)
+        tree_x = .248;
+        tree_y = -.987;
 
         obj.nav.gpsFeedbackNav(obj.gps, tree_x, tree_y);
 
-        arm.outoftheway();
-        rail.raise();
-        rail.dumpSnow(); 
-        rail.wiggle();
+        %arm.outoftheway();
+        %rail.raise();
+        %rail.dumpSnow(); 
+        %rail.wiggle();
         % typing clear all should stop motors etc from failing
         end
         
-        function taskthree = roof()
-        house_x = 0;
-        house_y = 0;
+        function taskthree = roof(obj)
+        house_x = 0.008;
+        house_y = 0.372;
 
         obj.nav.gpsFeedbackNav(obj.gps, house_x, house_y);
-        rail.lower();
-        arm.sweepRoof();
+        %rail.lower();
+        %arm.sweepRoof();
         end
     end
 end
